e85d35756e11e177f07c319c9d85857ec570e4c6
NiltonMorais <nilton_even@hotmail.com>
2017-06-14 15:20:45 -0300

    first commit

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use CodeBot\CallSendApi;
+use CodeBot\Message\Text;
+use CodeBot\SenderRequest;
+use CodeBot\WebHook;
+use Illuminate\Http\Request;
+
+class BotController extends Controller
+{
+    public function subscribe()
+    {
+        $webHook = new WebHook;
+        $subscribe = $webHook->check(config('botfb.validation_token'));
+        if(!$subscribe){
+            abort(403,'Unauthorized action.');
+        }
+        return $subscribe;
+    }
+
+    public function receiveMessage(Request $request)
+    {
+        $sender = new SenderRequest;
+        $senderId = $sender->getSenderId();
+        $message = $sender->getMessage();
+
+        $text = new Text($senderId);
+        $callSendApi = new CallSendApi(config('botfb.page_access_token'));
+        $callSendApi->make($text->message('Oii, eu sou um bot..'));
+        $callSendApi->make($text->message('Você digitou: '.$message));
+        return '';
+    }
+}
---
+++ config/botfb.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'page_access_token' => env('FB_PAGE_ACCESS_TOKEN'),
+    'validation_token' => env('FB_VALIDATION_TOKEN'),
+];
