f178db05b6e194bae7453821590c349b42ce57b5
NiltonMorais <nilton_even@hotmail.com>
2018-02-24 00:29:56 -0300

    criando o menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,18 +23,32 @@
 
                         <ul class="right hide-on-med-and-down">
                             <li>
-                                <a href="#" class="menu"><i class="material-icons">menu</i></a>
+                                <a id="nav-mobile" href="#" data-activates="slide-out" class="menu"><i class="material-icons">menu</i></a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </nav>
+        <ul id="slide-out" class="side-nav">
+            <li>
+                <div class="userView">
+                    <div class="background"><img src="img/leaf_background.jpg" alt="background"></div>
+                    <a><img src="https://secure.gravatar.com/avatar/0d2ed3be2bf553a1a36e8645310156f0" alt="Avatar" class="circle"></a>
+                    <a><span class="white-text name">Nilton Morais</span></a>
+                    <a href="https://github.com/NiltonMorais"><span class="white-text email">@NiltonMorais</span></a>
+                </div>
+            </li>
+        </ul>
+
        <div id="app">
        </div>
 
        <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+        <script>
+            $('#nav-mobile').sideNav();
+        </script>
        <script src="{{mix('js/app.js')}}"></script>
     </body>
 </html>
